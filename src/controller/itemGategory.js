2db500cd704f7ae9217d48c738a27713528101ff
fenil2502 <fenil9737@gmail.com>
2024-12-01 16:51:38 +0530

    HSNCode CRUD Operation

--- src/controller/itemGategory.js
+++ src/controller/itemGategory.js
@@ -1,3 +1,4 @@
+import HNSCodesScHema from "../model/hnsCode.js";
 import ItemCategory from "../model/itemCategory.js";
 
 
@@ -38,7 +39,46 @@ const deleteCategoryById = async (req, res) => {
     }
   };
 
+  const addEditHSNCode = async (req, res) => {
+    try {
+      let data = req.body.data
+      if (data._id && data._id.trim() !== '') {
+        const response = await HNSCodesScHema.findByIdAndUpdate(data._id, data, { new: true });
+        if (response) {
+          res.status(200).json({ Message: "HNSCode updated successfully", data: response });
+        } else {
+          res.status(404).json({ Message: "HNSCode not found" });
+        }
+      } else {
+        const response = new HNSCodesScHema(data);
+        await response.save();
+        res.status(200).json({ Message: "HNSCode added successfully", data: response });
+      }
+  
+    } catch (error) {
+      console.log("error in admin addEmployee controller", error);
+      res.status(500).json({ error: error.message });
+    }
+  };
+  
+  const deleteHSNCodeById = async (req, res) => {
+      try {
+        const { id } = req.query;
+        console.log(id)
+        let response = {}
+        if (id) {
+          response = await HNSCodesScHema.findByIdAndDelete(id, { IsDeleted: true }, { new: true, useFindAndModify: false });
+        }
+        res.status(201).json({ Message: "HNSCode has been deleted", responseContent: response });
+      } catch (error) {
+        console.log("error in item master controller", error);
+        res.status(500).json({ error: error.message });
+      }
+    };
+
 export {
   addEditItemGategory,
-  deleteCategoryById
+  deleteCategoryById,
+  addEditHSNCode,
+  deleteHSNCodeById
 };
--- src/routes/itemCategory.js
+++ src/routes/itemCategory.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { addEditItemGategory, deleteCategoryById } from "../controller/itemGategory.js";
+import { addEditHSNCode, addEditItemGategory, deleteCategoryById, deleteHSNCodeById } from "../controller/itemGategory.js";
 
 const itemCategoryRoute = express.Router();
 
 itemCategoryRoute.post("/itemCategory/AddEditItemGategory", addEditItemGategory);
 itemCategoryRoute.get("/itemCategory/DeleteCategoryById", deleteCategoryById);
+itemCategoryRoute.post("/itemCategory/AddEditHSNCode", addEditHSNCode);
+itemCategoryRoute.get("/itemCategory/DeleteHSNCodeById", deleteHSNCodeById);
 
 export default itemCategoryRoute;
