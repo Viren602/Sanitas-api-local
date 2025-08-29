e20b4219de0c67759e6d0bb037864c6fafac0742
Dhruvi1225 <dhruvibpatel62@gmail.com>
2025-08-29 16:32:16 +0530

    Dashboard changes

--- src/controller/dashboardController.js
+++ src/controller/dashboardController.js
@@ -226,15 +226,15 @@ const getMonthlySalesAndPurchase = async (req, res) => {
 
         // Define month order (April to March)
         const monthOrder = [
-            "April", "May", "June", "July", "August", "September",
-            "October", "November", "December", "January", "February", "March"
+            "Apr", "May", "June", "July", "Aug", "Sep",
+            "Oct", "Nov", "Dec", "Jan", "Feb", "Mar"
         ];
 
         const monthNameToNumber = {
-            January: 1, February: 2, March: 3,
-            April: 4, May: 5, June: 6, July: 7,
-            August: 8, September: 9, October: 10,
-            November: 11, December: 12
+            Jan: 1, Feb: 2, Mar: 3,
+            Apr: 4, May: 5, June: 6, July: 7,
+            Aug: 8, Sep: 9, Oct: 10,
+            Nov: 11, Dec: 12
         };
 
         const response = monthOrder.map(monthName => {
